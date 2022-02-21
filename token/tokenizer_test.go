1377ac2c986a359a2709565ad7c69106faefe949
Vladimir Sterlin <vsterlin21@gmail.com>
2022-02-21 15:58:33 -0800

    add if test

--- token/tokenizer_test.go
+++ token/tokenizer_test.go
@@ -24,3 +24,36 @@ func TestTokenizerError(t *testing.T) {
 	}
 
 }
+
+func TestTokenizeIf(t *testing.T) {
+	input := "if"
+
+	tokenizer := NewTokenizer(input)
+	tokens := tokenizer.Tokenize()
+	if !tokens[0].Equals(&IfToken{}) {
+		t.Error()
+	}
+
+}
+
+func TestAllKeywords(t *testing.T) {
+	type keywordTest struct {
+		Input          string
+		ExpectedResult Token
+	}
+
+	keywordTests := []keywordTest{
+		{"if", &IfToken{}}}
+
+	for _, test := range keywordTests {
+		t.Run(test.Input, func(t *testing.T) {
+			tokenizer := NewTokenizer(test.Input)
+			tokens := tokenizer.Tokenize()
+			actual := tokens[0]
+			if !actual.Equals(test.ExpectedResult) {
+				t.Errorf("expected token: \"%s\", got: %s", test.ExpectedResult, actual)
+			}
+		})
+	}
+
+}
