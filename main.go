804f255c3a3484f2cc718707784d7b1471e37d03
Vladimir Sterlin <vsterlin21@gmail.com>
2022-03-11 16:27:00 -0800

    remove stuff

--- main.go
+++ main.go
@@ -8,19 +8,9 @@ import (
 
 func main() {
 
-	// tokenizer := token.NewTokenizer("true false if ( ) { } else 100 var123 + - * % / || |> ^ const . && float = string print \"this is a String\"")
-	tokenizer := token.NewTokenizer("100")
-
+	tokenizer := token.NewTokenizer("true false if ( ) { } else 100 var123 + - * % / || |> ^ const . && float = string print \"this is a String\"")
 	tokens := tokenizer.Tokenize()
-	numToken := tokens[0]
 
-	anotherToken := &token.NumberToken{Number: 100}
-
-	fmt.Printf("numToken: %#v\n", numToken) // value 100
-	fmt.Printf("anotherToken: %v\n", anotherToken)
-	isEqual := numToken.Equals(anotherToken)
-
-	fmt.Printf("isEqual: %v\n", isEqual)
 	for _, t := range tokens {
 		fmt.Println(t)
 	}
