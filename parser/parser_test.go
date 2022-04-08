2afe2e6a6b4204d3a0a705697658a1cb37c4d415
sarkiscsun <sarkis.yapudzhian.63@my.csun.edu>
2022-04-08 13:41:35 -0700

    Multiply/divide parser test

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -40,3 +40,39 @@ func TestMinusOpExp(t *testing.T) {
 	}
 
 }
+
+func TestDivideOpTest(t *testing.T) {
+	first := NewOpExp(&NumberExp{1}, &DivideOp{}, &NumberExp{1})
+	second := NewOpExp(&NumberExp{1}, &DivideOp{}, &NumberExp{1})
+	third := NewOpExp(&NumberExp{1}, &DivideOp{}, &NumberExp{2})
+
+	fmt.Printf("%#v, %#v", first, second)
+	if !first.Equals(second) {
+		t.Error("Expected first and second operator expression to be equal")
+	}
+	if !first.Equals(first) {
+		t.Error("Expected first operator expression to be equal to itself")
+	}
+	if first.Equals(third) {
+		t.Error("Expected first operator expression not to be equal to third")
+	}
+
+}
+
+func TestMultiplyOpTest(t *testing.T) {
+	first := NewOpExp(&NumberExp{1}, &MultiplyOp{}, &NumberExp{1})
+	second := NewOpExp(&NumberExp{1}, &MultiplyOp{}, &NumberExp{1})
+	third := NewOpExp(&NumberExp{1}, &MultiplyOp{}, &NumberExp{2})
+
+	fmt.Printf("%#v, %#v", first, second)
+	if !first.Equals(second) {
+		t.Error("Expected first and second operator expression to be equal")
+	}
+	if !first.Equals(first) {
+		t.Error("Expected first operator expression to be equal to itself")
+	}
+	if first.Equals(third) {
+		t.Error("Expected first operator expression not to be equal to third")
+	}
+
+}
