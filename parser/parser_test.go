a1a402751d61a40d82e5433c4eb6156e9dbff654
sarkiscsun <sarkis.yapudzhian.63@my.csun.edu>
2022-04-08 13:50:04 -0700

    pow parser test

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -76,3 +76,21 @@ func TestMultiplyOpTest(t *testing.T) {
 	}
 
 }
+
+func TestPowOpTest(t *testing.T) {
+	first := NewOpExp(&NumberExp{1}, &PowOp{}, &NumberExp{1})
+	second := NewOpExp(&NumberExp{1}, &PowOp{}, &NumberExp{1})
+	third := NewOpExp(&NumberExp{1}, &PowOp{}, &NumberExp{2})
+
+	fmt.Printf("%#v, %#v", first, second)
+	if !first.Equals(second) {
+		t.Error("Expected first and second operator expression to be equal")
+	}
+	if !first.Equals(first) {
+		t.Error("Expected first operator expression to be equal to itself")
+	}
+	if first.Equals(third) {
+		t.Error("Expected first operator expression not to be equal to third")
+	}
+
+}
