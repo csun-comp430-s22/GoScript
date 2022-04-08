0a38078d4d5a927cbac02bd5ebd0d07bad5b0fa8
sarkiscsun <sarkis.yapudzhian.63@my.csun.edu>
2022-04-08 13:39:23 -0700

    minus op parser test

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -22,3 +22,21 @@ func TestPlusOpExp(t *testing.T) {
 	}
 
 }
+
+func TestMinusOpExp(t *testing.T) {
+	first := NewOpExp(&NumberExp{1}, &MinusOp{}, &NumberExp{1})
+	second := NewOpExp(&NumberExp{1}, &MinusOp{}, &NumberExp{1})
+	third := NewOpExp(&NumberExp{1}, &MinusOp{}, &NumberExp{2})
+
+	fmt.Printf("%#v, %#v", first, second)
+	if !first.Equals(second) {
+		t.Error("Expected first and second operator expression to be equal")
+	}
+	if !first.Equals(first) {
+		t.Error("Expected first operator expression to be equal to itself")
+	}
+	if first.Equals(third) {
+		t.Error("Expected first operator expression not to be equal to third")
+	}
+
+}
