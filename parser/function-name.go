875d65ca622e9f228bef6fe871571cae7101c97f
Anthony Magana <anthonymagana0705@gmail.com>
2022-04-08 13:20:42 -0700

    function name

---
+++ parser/function-name.go
@@ -0,0 +1,24 @@
+package parser
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type FunctionName struct {
+	Name string
+}
+
+func NewFunctionName(name string) *FunctionName {
+	return &FunctionName{Name: name}
+}
+
+func (FN *FunctionName) Equals(other interface{}) bool {
+	castOther := reflect.ValueOf(other)
+	otherFunctionName, ok := reflect.Indirect(castOther).Interface().(FunctionName)
+	return ok && reflect.TypeOf(other) == reflect.TypeOf(FN) && FN.Name == otherFunctionName.Name
+}
+
+func (FN *FunctionName) String() string {
+	return fmt.Sprintf("FunctionName(%s)", FN.Name)
+}
