4c309a03edc9bbb0d839a4eb70c88df65973a912
Tristin Greenstein <tristingreenstein@mail.com>
2022-04-08 18:05:21 -0700

    Update parser_test.go

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -347,3 +347,20 @@ func TestOrOpMod(t *testing.T) {
 	}
 
 }
+
+func TestLessThanOp(t *testing.T) {
+	first := NewOpExp(&NumberExp{1}, &LessOp{}, &NumberExp{1})
+	second := NewOpExp(&NumberExp{1}, &LessOp{}, &NumberExp{1})
+	third := NewOpExp(&NumberExp{1}, &LessOp{}, &NumberExp{2})
+
+	fmt.Printf("%#v, %#v", first, second)
+	if !first.Equals(second) {
+		t.Error("Expected first and second operator expression to be equal")
+	}
+	if !first.Equals(first) {
+		t.Error("Expected first operator expression to be equal to itself")
+	}
+	if first.Equals(third) {
+		t.Error("Expected first operator expression not to be equal to third")
+	}
+}
