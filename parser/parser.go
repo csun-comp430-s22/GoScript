f514bac6baa2c5485aa90ff23e38442aabc15217
snazcsun <stephan.nazarian.47@my.csun.edu>
2022-04-08 18:23:24 -0700

    and parser test

--- parser/parser.go
+++ parser/parser.go
@@ -83,6 +83,8 @@ func (p *Parser) ParseAdditiveOp(position int) (*ParseResult[Operator], error) {
 		return NewParseResult[Operator](&PipeOp{}, position+1), nil
 	} else if _, ok := tkn.(*token.OrToken); ok {
 		return NewParseResult[Operator](&OrOp{}, position+1), nil
+	} else if _, ok := tkn.(*token.AndToken); ok {
+		return NewParseResult[Operator](&AndOp{}, position+1), nil
 	} else {
 		return nil, NewParserError("err")
 	}
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -364,3 +364,35 @@ func TestLessThanOp(t *testing.T) {
 		t.Error("Expected first operator expression not to be equal to third")
 	}
 }
+
+func TestAndOp(t *testing.T) {
+	first := NewOpExp(&NumberExp{1}, &AndOp{}, &NumberExp{1})
+	second := NewOpExp(&NumberExp{1}, &AndOp{}, &NumberExp{1})
+	third := NewOpExp(&NumberExp{1}, &AndOp{}, &NumberExp{2})
+
+	fmt.Printf("%#v, %#v", first, second)
+	if !first.Equals(second) {
+		t.Error("Expected first and second operator expression to be equal")
+	}
+	if !first.Equals(first) {
+		t.Error("Expected first operator expression to be equal to itself")
+	}
+	if first.Equals(third) {
+		t.Error("Expected first operator expression not to be equal to third")
+	}
+}
+
+func TestAmpersandOpAnd(t *testing.T) {
+	tokens := []token.Token{&token.AndToken{}}
+	parser := NewParser(tokens)
+	parseResult, err := parser.ParseAdditiveOp(0)
+	if err != nil {
+		t.Error("Unexpected parser error")
+	}
+
+	if !parseResult.Equals(NewParseResult[Operator](&AndOp{}, 1)) {
+		t.Error("Expected parse result did not equal actual")
+
+	}
+
+}
