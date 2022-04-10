834beea26cba06481f96fcb1e7d4c3c0a0621c17
Anthony Magana <anthonymagana0705@gmail.com>
2022-04-10 16:05:41 -0700

    adds tests for or op and or exp

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -609,7 +609,6 @@ func TestComparisonOpInvalid(t *testing.T) {
 // 	return NewParseResult[Operator](&NegateOp{}, position+1), nil
 
 func TestLogicalExpAnd(t *testing.T) {
-	// tokens := []token.Token{&token.TrueToken{}, &token.AndToken{}, &token.TrueToken{1}}
 	tokens := []token.Token{&token.NumberToken{Number: 1}, &token.AndToken{}, &token.NumberToken{Number: 1}}
 
 	parser := NewParser(tokens)
@@ -617,6 +616,31 @@ func TestLogicalExpAnd(t *testing.T) {
 
 	if !parseResult.Equals(NewParseResult[Exp](NewOpExp(&NumberExp{1}, &AndOp{}, &NumberExp{1}), 3)) {
 
+		t.Error("Parse result did not equal the expected result")
+	}
+}
+
+func TestLogicalOpOr(t *testing.T) {
+	tokens := []token.Token{&token.OrToken{}}
+	parser := NewParser(tokens)
+	parseResult, err := parser.ParseLogicalOp(0)
+	if err != nil {
+		t.Error("Unexpected parser error")
+	}
+
+	if !parseResult.Equals(NewParseResult[Operator](&OrOp{}, 1)) {
+		t.Error("Expected parse result did not equal actual")
+
+	}
+}
+
+func TestLogicalExpOr(t *testing.T) {
+	tokens := []token.Token{&token.NumberToken{Number: 1}, &token.OrToken{}, &token.NumberToken{Number: 1}}
+
+	parser := NewParser(tokens)
+	parseResult, _ := parser.ParseLogicalExp(0)
+
+	if !parseResult.Equals(NewParseResult[Exp](NewOpExp(&NumberExp{1}, &OrOp{}, &NumberExp{1}), 3)) {
 		t.Error("Parse result did not equal the expected result")
 	}
 }
